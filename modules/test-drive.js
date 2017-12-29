395716384ece71a7465de1775a3ec40d84d2c09c
YanTing1111 <1178170499@qq.com>
2017-12-29 19:08:35 +0800

    'wxapp-starter'

---
+++ modules/test-drive.js
@@ -0,0 +1,7 @@
+const testDrive = () => {
+    wx.showToast({
+        title:'请联系当地经销商'
+    });
+}
+
+export default testDrive; //向外界输出testDrive函数
---
+++ pages/index/index.js
@@ -0,0 +1,24 @@
+import testDrive from '../../modules/test-drive';
+console.log(testDrive);
+const app = getApp();
+
+Page({
+  data: {
+    slides:null
+  },
+  onLoad () {
+    // 全局数据 globalData
+    this.setData({
+      slides:app.globalData.slides
+    })
+  },
+  testDrive,
+  readMore(event){
+    let id = event.target.dataset.id;
+    console.log();
+    wx.navigateTo({
+      // queryString 查询字符串 ?id
+      url:`/pages/vehicles/show?id=${id}`
+    })
+  }
+})
